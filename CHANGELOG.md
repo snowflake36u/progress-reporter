1498a53ec3b48c03093f5431012f696e87037fcc
semantic-release <semantic-release>
2026-08-24 22:47:18 +0900

    0.2.0
    Automatically generated by python-semantic-release

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG
+
+<!-- version list -->
+
+## v0.2.0 (2026-08-24)
+
+- Initial Release
--- src/progress_reporter/__init__.py
+++ src/progress_reporter/__init__.py
@@ -16,4 +16,4 @@ __all__ = [
 	"ProgressSession",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
